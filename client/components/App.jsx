304a96cb026d8acf4027bb1329da54d1b23db065
Jonty-Buchan <jontyterrence@gmail.com>
2020-08-04 21:05:21 +1200

    Adds initial nav bar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
-import request from 'superagent'
 
 import Nav from './Nav'
-import Hero from './Hero'
-import Listing from './Listing'
-import Map from './Map'
-import CategorySelector from './Category-Selector'
-import InfoBoxes from './InfoBoxes'
-import Footer from './Footer'
 
 class App extends React.Component {
   render () {
     return (
       <div className="body">
         <Nav />
-        <Hero />
-        <Listing />
-        <Map />
-        <CategorySelector />
-        <InfoBoxes />
-        <Footer />
       </div>
     )
   }
