f3ea23e7f6af5786a69127a873cb9001b2e6d5e3
Jonty-Buchan <jontyterrence@gmail.com>
2020-08-04 21:40:46 +1200

    Implements hero video

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Nav from './Nav'
+import Hero from './Hero'
 
 class App extends React.Component {
   render () {
     return (
       <div className="body">
         <Nav />
+        <Hero />
       </div>
     )
   }
