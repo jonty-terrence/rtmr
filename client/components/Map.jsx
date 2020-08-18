da5667ab55758ad3db4dc1709a2b61f41d14cb91
Jonty-Buchan <jontyterrence@gmail.com>
2020-08-18 15:45:17 +1200

    Initialises base map with generic public access token

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ReactMapGL from 'react-map-gl'
 
 function Map () {
+    const [viewport, setViewport] = useState({
+        latitude: -36.848461,
+        longitude: 174.763336,
+        width: "100vw",
+        height: "100vh",
+        zoom: 10
+    })
   return (
     <div className="map-box">
-      <h5>this is the map component</h5>
+      <ReactMapGL 
+      {...viewport}
+      mapboxApiAccessToken="pk.eyJ1Ijoiam9udHl0ZXJyZW5jZSIsImEiOiJja2R6ZG55NzMxYm00MnFvYTQzeGkzZDZ4In0.lI5DIs4hAE1MSBURbIwnFA">
+          </ReactMapGL> 
     </div>
   )
 }
