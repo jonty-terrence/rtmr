a88d29712c14da609a1f8d7ae32e69a379583a7a
Jonty-Buchan <jontyterrence@gmail.com>
2020-08-19 14:29:11 +1200

    Adds scrolling to listing component

--- client/components/Listings.jsx
+++ client/components/Listings.jsx
@@ -6,13 +6,12 @@ import businesses from '../data/auckland-businesses'
 function Listings () {
   return (
     <div className="listing-container">
+      <h2 className="listing-container-title">Auckland Businesses</h2>
       {businesses.map(business => {
         return (
-          <div>
-            <img src={business.imageUrl}></img>
-            <h4>{business.name}</h4>
-            <p>{business.url}</p>
-            
+          <div key={business.name} className="business-listing">
+            <a href={business.url}><img className="business-image" src={business.imageUrl}></img></a>
+            <h4 className="business-name">{business.name}</h4>
           </div>
         )
       })}
