aafa34e63cb1f9db4a7de6def6e9b7dd467385d8
Jonty-Buchan <jontyterrence@gmail.com>
2020-10-13 17:15:50 +1300

    Adds information about The Living Wage

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Nav from './Nav'
 import Hero from './Hero'
 import MapContainer from'./MapContainer'
+import InfoBoxes from './InfoBoxes'
 
 class App extends React.Component {
   render () {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <Nav />
         <Hero />
         <MapContainer />
+        <InfoBoxes />
       </div>
     )
   }
