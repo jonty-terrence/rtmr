f800a6d6bc8758aadf36b49221dba7d7fbae4d0e
Jonty-Buchan <jontyterrence@gmail.com>
2020-10-13 17:48:20 +1300

    Initialises layout for bottom section

--- client/components/InfoBoxes.jsx
+++ client/components/InfoBoxes.jsx
@@ -4,6 +4,7 @@ function InfoBoxes () {
   return (
     <div className="info-boxes">
       <div className="first-about">
+        <div className="text-box">
         <h3>The Living Wage</h3>
         <p>
           "A living wage is the income necessary to provide workers and their families with the basic 
@@ -12,9 +13,12 @@ function InfoBoxes () {
           as a whole to strive for a living wage for all households as a necessary and important step in the 
           reduction of poverty in New Zealand.”
         </p>
-        <img src="market" alt="market"/>
+        </div>
+        <img src="market.jpg" alt="market"/>
       </div>
       <div className="second-about">
+      <img src="bookstore.jpg" alt="book store"/>
+        <div className="text-box">
         <h3>Support The Living Wage Aotearoa</h3>
         <p>
         The Living Wage has emerged as a response to growing poverty and inequality that continues to hold back so 
@@ -23,7 +27,7 @@ function InfoBoxes () {
         to campaign for a Living Wage.<br></br>
         <a href="https://www.livingwage.org.nz/">Find out more about The Living Wage Aotearoa and view a full list of accredited employers here</a>
         </p>
-        <img src="bookstore.jpg" alt="book store"/>
+        </div>
       </div>
     </div>
   )
