2c93064a12abed583d7c43b4d03e3b85d5e2ca40
Jonty-Buchan <jontyterrence@gmail.com>
2020-10-28 14:52:55 +1300

    Fixes console errors

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -25,8 +25,8 @@ function Map () {
                 if (business.lat) {
                     return (
                         <Marker 
-                            latitude={Number(business.lat)} longitude={Number(business.long)} offsetLeft={-20} offsetTop={-10}>
-                            <i class="fas fa-map-marker"></i>
+                            latitude={Number(business.lat)} longitude={Number(business.long)} offsetLeft={-20} offsetTop={-10} key={business.streetNum}>
+                            <i className="fas fa-map-marker"></i>
                         </Marker>
                     )
                 }
